c1d12f8ec482d012c76ec8e730396e7132ebf59e
Jason Michels <jmichels@fisdap.net>
2017-12-07 15:24:57 -0600

    Updated to check and make sure version does not exist already

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 In progress, but eventually a cli to publish versioned front end sections of websites, for single page app, as well as legacy applications.
 
 See example journey.json file and the required command line arguments 
+
+### Example Usage
+```sh
+$ journey-cli -journey=journey.json -cmd=publish -bucket=nameOfBucket
+```
--- main.go
+++ main.go
@@ -12,23 +12,17 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"gopkg.in/go-playground/validator.v9"
 )
 
 var journey Journey
-var assetManifest AssetManifest
+var assets map[string]string
 var validate *validator.Validate
 var journeyPath *string
 
 const publish = "publish"
-
-type AssetManifest struct {
-	MainJS     string `json:"main.js"`
-	MainJSMap  string `json:"main.js.map"`
-	MainCSS    string `json:"main.css"`
-	MainCSSMap string `json:"main.css.map"`
-}
 
 func loadAssetManifestConfig(path string) error {
 	abs, err := filepath.Abs(path)
@@ -41,7 +35,7 @@ func loadAssetManifestConfig(path string) error {
 		return err
 	}
 
-	return json.Unmarshal(content, &assetManifest)
+	return json.Unmarshal(content, &assets)
 }
 
 func loadJourneyConfig(path string) error {
@@ -84,55 +78,80 @@ func (j *Journey) GetAssetKey(path string) string {
 	return j.Name + "/" + j.Version + "/" + path
 }
 
-func (j *Journey) Publish() (bool, error) {
+// ValidateVersionNotUsed Validate that the version is not already in use, we dont want to publish over something
+func (j *Journey) ValidateVersionNotUsed(sess *session.Session) (bool, error) {
+	svc := s3.New(sess)
+	input := &s3.HeadObjectInput{
+		Bucket: aws.String(j.Bucket),
+		Key:    aws.String(j.Name + "/" + j.Version + "/journey.json"),
+	}
+
+	_, err := svc.HeadObject(input)
+	if err != nil {
+		// I know we are returning ok, but if no item is found we can assume the version does not exist
+		return true, err
+	}
+
+	return false, fmt.Errorf("Version %v/%v already exists, publishing failed", j.Name, j.Version)
+}
+
+// Publish Publish the assets using the journey configuration
+func (j *Journey) Publish() error {
 	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
 	if err != nil {
-		fmt.Println("Error creating AWS session ", err)
-		return false, err
+		log.Println("Error creating AWS session ", err)
+		return err
 	}
 
 	// check to make sure a directory in S3 does not exist with the Version
-
-	// if the directory does not exist, load the asset-manifest file into a struct
-	// loop through each item in the asset manifest and upload the files
-	// copy into {bucket}/{name}/{version}/
-	if err := loadAssetManifestConfig(j.Manifest); err != nil {
-		return false, err
+	if ok, err := j.ValidateVersionNotUsed(sess); !ok {
+		return err
 	}
+	log.Printf("Version %v/%v is NOT being used already", j.Name, j.Version)
+
+	if err := loadAssetManifestConfig(j.Manifest); err != nil {
+		return err
+	}
+	log.Println("Successfully loaded Asset Manifest configuration")
 
 	// Create an uploader with the session and default options
 	uploader := s3manager.NewUploader(sess)
 
+	log.Printf("Getting ready to upload %v files...", len(assets)+2)
 	var wg sync.WaitGroup
-	wg.Add(6)
-	go uploadToS3(j.Bucket, j.GetAssetPath(assetManifest.MainCSS), j.GetAssetKey(assetManifest.MainCSS), uploader, &wg)
-	go uploadToS3(j.Bucket, j.GetAssetPath(assetManifest.MainCSSMap), j.GetAssetKey(assetManifest.MainCSSMap), uploader, &wg)
-	go uploadToS3(j.Bucket, j.GetAssetPath(assetManifest.MainJS), j.GetAssetKey(assetManifest.MainJS), uploader, &wg)
-	go uploadToS3(j.Bucket, j.GetAssetPath(assetManifest.MainJSMap), j.GetAssetKey(assetManifest.MainJSMap), uploader, &wg)
+	wg.Add(len(assets) + 2)
+
+	for _, v := range assets {
+		go uploadToS3(j.Bucket, j.GetAssetPath(v), j.GetAssetKey(v), uploader, &wg)
+	}
 
 	// make sure to put the journey.json, and asset-manifest.json file into {bucket}/{name}/{version}/
 	go uploadToS3(j.Bucket, j.Manifest, j.GetAssetKey("asset-manifest.json"), uploader, &wg)
 	go uploadToS3(j.Bucket, *journeyPath, j.GetAssetKey("journey.json"), uploader, &wg)
 	wg.Wait()
 
-	return true, nil
+	return nil
 }
 
 // uploadToS3 Take a file path and key and upload to S3
 func uploadToS3(bucket string, path string, key string, uploader *s3manager.Uploader, wg *sync.WaitGroup) (*s3manager.UploadOutput, error) {
 	defer wg.Done()
+	log.Printf("Starting to upload %v, at this path: %v, to this bucket: %v", key, path, bucket)
 
 	if len(path) <= 0 {
+		log.Printf("Key: %v, does not have a path and will not be uploaded", key)
 		return nil, nil
 	}
 
 	abs, err := filepath.Abs(path)
 	if err != nil {
+		log.Printf("Key: %v, had an issue getting absolute file path and was not uploaded", key)
 		return nil, err
 	}
 
 	f, err := os.Open(abs)
 	if err != nil {
+		log.Printf("Key: %v, was unable to be opened and will not be uploaded", key)
 		return nil, err
 	}
 	defer f.Close()
@@ -147,29 +166,32 @@ func uploadToS3(bucket string, path string, key string, uploader *s3manager.Uplo
 
 func main() {
 
-	// add getting bucket from a flag or environment
-
 	journeyPath = flag.String("journey", "journey.json", "Location of the journey.json file")
 	cmd := flag.String("cmd", publish, "Command to invoke, eg: publish")
 	bucket := flag.String("bucket", "", "AWS S3 bucket")
 	flag.Parse()
 
 	if err := loadJourneyConfig(*journeyPath); err != nil {
-		panic(err)
+		log.Panic(err)
 	}
+	log.Println("Successfully loaded journey.json configuration")
 
 	journey.Bucket = *bucket
 
 	validate = validator.New()
 	if err := journey.Validate(); err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
-	//Call the command, right now just publish
 	switch *cmd {
 	case publish:
-		journey.Publish()
+		if err := journey.Publish(); err != nil {
+			log.Panic(err)
+		}
+		log.Println("Finished publishing all assets to S3")
 	default:
-		log.Println("Do not recognize command")
+		log.Fatalf("Do not recognize command: %v", *cmd)
 	}
+
+	log.Println("Continue with your Journey!")
 }
